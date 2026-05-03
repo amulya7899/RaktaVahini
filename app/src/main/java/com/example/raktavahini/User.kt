3a507795ceae0f54a7ffca750cba9a3f0de81493
amul7899 <amulyad.22ise@cambridge.edu.in>
2026-05-03 23:14:22 +0530

    Initial working version of RaktaVahini app

--- app/src/main/java/com/example/raktavahini/User.kt
+++ app/src/main/java/com/example/raktavahini/User.kt
@@ -5,5 +5,8 @@ data class User(
     val blood: String,
     val location: String,
     val date: Long,
-    val available: Boolean
+    val available: Boolean,
+    val phone: String,
+    val latitude: Double,
+    val longitude: Double
 )
